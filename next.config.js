e1975faa0cc85a454faec95bf8550cd9fee1ccba
Scott Cho <scottsgcho@gmail.com>
2021-02-17 04:41:07 +0900

    env var client config change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withCSS = require('@zeit/next-css');
 require('dotenv').config()
 const Dotenv = require('dotenv-webpack')
+const webpack = require('webpack');
 
 const path = require('path')
 
@@ -8,19 +9,28 @@ const path = require('path')
 //Allowing use of .css files for react-draft-wysiwyg module
 module.exports = withCSS({
     webpack: (config, options) => {
+        const env = Object.keys(process.env).reduce((acc, curr) => {
+            acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+            return acc;
+        }, {});
+
+        // Fixes npm packages that depend on `fs` module
+        config.node = {
+            fs: 'empty'
+        };
 
         config.plugins = config.plugins || []
+        config.plugins.push(new webpack.DefinePlugin(env));
 
-        config.plugins = [
-            ...config.plugins,
+        // config.plugins = [
+        //     ...config.plugins,
 
-            // Read the .env file
-            new Dotenv({
-                path: path.join(__dirname, '.env'),
-                systemvars: true
-            })
-        ]
-
+        //     // Read the .env file
+        //     new Dotenv({
+        //         path: path.join(__dirname, '.env'),
+        //         systemvars: true
+        //     })
+        // ]
         return config
     }
 });
