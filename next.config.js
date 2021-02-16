84fa4518f978200dfbb63cf444ae3e28dcd41b68
Scott Cho <scottsgcho@gmail.com>
2021-02-17 04:58:22 +0900

    nextconfig env bug fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withCSS = require('@zeit/next-css');
-require('dotenv').config()
-const Dotenv = require('dotenv-webpack')
+if (process.env.NODE_ENV != 'production') {
+    require('dotenv').config({path: __dirname + '/.env'})
+}
 const webpack = require('webpack');
 
 const path = require('path')
@@ -9,6 +10,7 @@ const path = require('path')
 //Allowing use of .css files for react-draft-wysiwyg module
 module.exports = withCSS({
     webpack: (config, options) => {
+        console.log("PENV: ", process.env)
         const env = Object.keys(process.env).reduce((acc, curr) => {
             acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
             return acc;
